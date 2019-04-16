9f29ffa61df33ec4916c6d775a29cb6d44a4546b
SofiaRuizHinojosa27 <srh_19@hotmail.com>
2019-04-16 09:02:04 -0500

    Se realizaron los test para las funciones de que si existe un archivo, si es valido y si es archivo con ext MD, las funciones tambien se agregaron, y los tests si pasaron

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-module.exports = () => {
-  // ...
+const fs = require("fs");
+const path= require("path");
+
+module.exports =  {
+
+pathCheck: function(pathfile){
+  if(pathfile == undefined){
+    return false
+  }
+  else{
+    return true
+}
+},
+
+pathExist: function(pathfile){
+  if (fs.existsSync(pathfile)){
+    return true;
+  }
+  else{
+    return false;
+  }
+},
+
+// pathDirectory: function(pathfile){
+//   if (fs.statSync(pathfile).isDirectory()){
+//     return true;
+//   } else{
+//     return false;
+//   }
+// },
+
+pathMD: function(pathfile){
+  if(path.extname(pathfile) === ".md"){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+// pathAbsolute: function(pathfile){
+//   if(path.isAbsolute(pathfile)){
+//     return true;
+//   }
+//   else{
+//     return false;
+//   }
+//   }
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,3 +1,4 @@
+const index= require('../index.js');
 const mdLinks = require('../');
 
 
@@ -8,3 +9,47 @@ describe('mdLinks', () => {
   });
 
 });
+
+describe ("pathCheck", ()=>{
+  it("should return false", ()=>{
+    expect(index.pathCheck()).toBe(false);
+  });
+  it("should return true", ()=>{
+    expect(index.pathCheck("./README.md")).toBe(true);
+});
+});
+
+describe( "pathExist", ()=>{
+  it("should be false", () =>{
+    expect(index.pathExist("./README.txt")).toBe(false);
+  });
+it("Should be true", ()=>{
+  expect(index.pathExist("./README.md")).toBe(true);
+});
+});
+
+// describe("pathAbsolute", ()=>{
+//   it("should return true", ()=>{
+//     expect(index.pathAbsolute("")).toBe(true);
+//   });
+// });
+
+
+
+describe ("pathMD", ()=>{
+  it("should be true",()=>{
+    expect(index.pathMD("./README.md")).toBe(true);
+  });
+  it("should return false", ()=>{
+    expect(index.pathMD("./index.js")).toBe(false);
+});
+
+});
+// describe ("pathDirectory", ()=>{
+//   it("Should be a directory",()=>{
+//     expect(index.pathDirectory("/Users/srh_1/Desktop/GDL002-md-links/test"‪)).toBe(true);
+//   });
+//   it("should be a file",()=>{
+//     expect(index.pathDirectory("../GDL002-md-links/index.js")).toBe(false);
+//   })
+// });
